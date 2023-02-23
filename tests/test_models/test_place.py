814a1f30aa8257679491edbb2f0a7e9586ca227d
wisdomdzontoh <wisdomdzontoh@gmail.com>
2023-02-23 03:31:58 -0800

    tasks

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python3
+"""
+Contains the TestPlaceDocs classes
+"""
+
+from datetime import datetime
+import inspect
+import models
+from models import place
+from models.base_model import BaseModel
+import pep8
+import unittest
+Place = place.Place
+
+
+class TestPlaceDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Place class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.place_f = inspect.getmembers(Place, inspect.isfunction)
+
+    def test_pep8_conformance_place(self):
+        """Test that models/place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_place(self):
+        """Test that tests/test_models/test_place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_place_module_docstring(self):
+        """Test for the place.py module docstring"""
+        self.assertIsNot(place.__doc__, None,
+                         "place.py needs a docstring")
+        self.assertTrue(len(place.__doc__) >= 1,
+                        "place.py needs a docstring")
+
+    def test_place_class_docstring(self):
+        """Test for the Place class docstring"""
+        self.assertIsNot(Place.__doc__, None,
+                         "Place class needs a docstring")
+        self.assertTrue(len(Place.__doc__) >= 1,
+                        "Place class needs a docstring")
+
+    def test_place_func_docstrings(self):
+        """Test for the presence of docstrings in Place methods"""
+        for func in self.place_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
+
+
+class TestPlace(unittest.TestCase):
+    """Test the Place class"""
+    def test_is_subclass(self):
+        """Test that Place is a subclass of BaseModel"""
+        place = Place()
+        self.assertIsInstance(place, BaseModel)
+        self.assertTrue(hasattr(place, "id"))
+        self.assertTrue(hasattr(place, "created_at"))
+        self.assertTrue(hasattr(place, "updated_at"))
+
+    def test_city_id_attr(self):
+        """Test Place has attr city_id, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.city_id, None)
+        else:
+            self.assertEqual(place.city_id, "")
+
+    def test_user_id_attr(self):
+        """Test Place has attr user_id, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "user_id"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.user_id, None)
+        else:
+            self.assertEqual(place.user_id, "")
+
+    def test_name_attr(self):
+        """Test Place has attr name, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "name"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.name, None)
+        else:
+            self.assertEqual(place.name, "")
+
+    def test_description_attr(self):
+        """Test Place has attr description, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "description"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.description, None)
+        else:
+            self.assertEqual(place.description, "")
+
+    def test_number_rooms_attr(self):
+        """Test Place has attr number_rooms, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_rooms"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.number_rooms, None)
+        else:
+            self.assertEqual(type(place.number_rooms), int)
+            self.assertEqual(place.number_rooms, 0)
+
+    def test_number_bathrooms_attr(self):
+        """Test Place has attr number_bathrooms, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.number_bathrooms, None)
+        else:
+            self.assertEqual(type(place.number_bathrooms), int)
+            self.assertEqual(place.number_bathrooms, 0)
+
+    def test_max_guest_attr(self):
+        """Test Place has attr max_guest, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "max_guest"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.max_guest, None)
+        else:
+            self.assertEqual(type(place.max_guest), int)
+            self.assertEqual(place.max_guest, 0)
+
+    def test_price_by_night_attr(self):
+        """Test Place has attr price_by_night, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "price_by_night"))
+        if models.storage_t == 'db':
+            self.assertEqual(place.price_by_night, None)
+        else:
+            self.assertEqual(type(place.price_by_night), int)
+            self.assertEqual(place.price_by_night, 0)
+
+    def test_latitude_attr(self):
+        """Test Place has attr latitude, and it's a float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "latitude"))
+        if models.storage_t == 'db':
+
